d914e6b70d912983fe64ef0bb384cc24813b1c35
Joe Barnes <barnesjd@gmail.com>
2015-04-13 11:12:28 -0500

    Printing basic module info in alphabetical order by org and name

--- src/main/scala/net/virtualvoid/sbt/graph/Csv.scala
+++ src/main/scala/net/virtualvoid/sbt/graph/Csv.scala
@@ -2,6 +2,12 @@ package net.virtualvoid.sbt.graph
 
 object Csv {
   def toCsv(graph: IvyGraphMLDependencies.ModuleGraph):String = {
-    "Hello"
+    graph.modules.toList.sortBy { case (id, module) =>
+        (id.organisation, id.name)
+      }
+//      .map { case (id, module) =>
+//
+//      }
+      .map(_._1.idString).mkString("\n")
   }
 }
